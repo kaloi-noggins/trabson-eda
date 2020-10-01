bc52d9578a0e31b4abe3414fac16ba3e180b5912
JaasielAbner <jabner101@gmail.com>
2020-09-30 22:07:52 -0700

    Deletando definitivamente a linkedlist do projeto

--- main.c
+++ main.c
@@ -11,19 +11,19 @@ int main(){
       hashmap_set(map, "sit", 4);
       hashmap_set(map, "amet", 5);
 
-      //printf("map['%s'] = %i\n", "lorem", hashmap_get(map, "lorem"));
-      //printf("map['%s'] = %i\n", "ipsum", hashmap_get(map, "ipsum"));
+      printf("map['%s'] = %i\n", "lorem", hashmap_get(map, "lorem"));
+      printf("map['%s'] = %i\n", "ipsum", hashmap_get(map, "ipsum"));
       printf("map['%s'] = %i\n", "amet", hashmap_get(map, "amet"));
 
-      //printf("has('%s') = %i\n", "amet", hashmap_has(map, "amet"));
-      //printf("has('%s') = %i\n", "pedro", hashmap_has(map, "pedro"));
+      printf("has('%s') = %i\n", "amet", hashmap_has(map, "amet"));
+      printf("has('%s') = %i\n", "pedro", hashmap_has(map, "pedro"));
 
-      //printf("size: %i\n", hashmap_size(map));
+      printf("size: %i\n", hashmap_size(map));
 
-      //hashmap_remove(map, "lorem");
-      //printf("size: %i\n", hashmap_size(map));
+      hashmap_remove(map, "lorem");
+      printf("size: %i\n", hashmap_size(map));
 
-      //hashmap_delete(map);
+      hashmap_delete(map);
 
       return EXIT_SUCCESS;
 }
